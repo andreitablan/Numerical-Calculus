f63cf76acee4c48eae9dfb1def9dc8807bf75c88
Andrie Tablan <andreitablan01@gmail.com>
2023-02-25 09:15:47 +0200

    Update main.py

--- Tema1/main.py
+++ Tema1/main.py
@@ -1,10 +1,12 @@
 # FII - 3A5 - Leagan Dan & Tablan Andrei
+import numpy as np
 
-def ex_1():
-    return
+
+def ex_1(u):
+    while (1 + u) != 1:
+        u = u / 10
+    return u
 
 
 if __name__ == '__main__':
-    ex_1()
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    print(ex_1(1))
