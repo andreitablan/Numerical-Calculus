be5d7000e5d869eb88a4a0270bbafa97ca80ca3c
Andrie Tablan <andreitablan01@gmail.com>
2023-03-01 20:51:09 +0200

    Update main.py

--- Tema1/main.py
+++ Tema1/main.py
@@ -8,5 +8,33 @@ def ex_1(u):
     return u
 
 
+def ex_2a(x, y, z):
+    return (x + y) + z == x + (y + z)
+
+
+def multiply(a, b):
+    return a * b
+
+
+def verify(x, y, z):
+    a = multiply(multiply(x, y), z)
+    b = multiply(x, multiply(y, z)),
+    return a, b, a == b
+
+
+def ex_2b(u):
+    x = u
+    y = np.random.uniform(0.0, 10.2)
+    z = np.random.uniform(0.0, 10.2)
+    print("The numbers are: ", x, y, z)
+    return verify(x, y, z)
+
+
 if __name__ == '__main__':
-    print(ex_1(1))
+    # ex 1
+    u = ex_1(1)
+    # print(u)
+
+    # ex 2
+    # print("The answer for (x+y)+z==x+(y+z) is ", ex_2a(1.0, u, u))
+    print("The answers for (xy)z and x(yz) are:", ex_2b(u))
