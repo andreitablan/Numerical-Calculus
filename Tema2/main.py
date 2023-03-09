5efd31ddbd35ec9f3aa511522783a28cf2f5314f
Andrie Tablan <andreitablan01@gmail.com>
2023-03-09 16:51:31 +0200

    Update main.py

--- Tema2/main.py
+++ Tema2/main.py
@@ -10,13 +10,13 @@ def machine_epsilon():
 def lower_triangular(A):
     n = len(A)
     L = np.zeros((n, n))
-    for j in range(n):
-        for i in range(j, n):
-            if i == j:
-                L[i][j] = np.sqrt(A[i][j] - sum(L[i][k] ** 2 for k in range(j)))
+    for i in range(n):
+        for j in range(i + 1):
+            s = sum(L[i][k] * L[j][k] for k in range(j))
+            if (i == j):
+                L[i][j] = np.sqrt(max(A[i][i] - s, 0))
             else:
-                L[i][j] = (A[i][j] - sum(L[i][k] * L[j][k] for k in range(j))) / L[j][j]
-
+                L[i][j] = (1.0 / L[j][j] * (A[i][j] - s))
     return L
 
 
@@ -34,35 +34,77 @@ def get_transpose(A):
 
 
 def get_diagonal_matrix(A):
-    return np.diag(A)
+    return np.diag(np.diag(A))
 
 
 def multiply_matrices(A, B):
     return A.dot(B)
 
 
-def cholesky_with_diagonal_out(A):
+def cholesky_decomposition(A):
     n = len(A)
-    L = np.linalg.cholesky(A)
-    d = np.diag(L)
-    L_prime = np.identity(n) + L - np.diag(d)
-    D = np.diag(d)
-    print(L)
-    print(np.matmul(L_prime, D))
+    L = [[0.0] * n for i in range(n)]
+    D = [0.0] * n
+
+    for i in range(n):
+        for j in range(i):
+            s = 0.0
+            for k in range(j):
+                s += L[i][k] * L[j][k] * D[k]
+            L[i][j] = (A[i][j] - s) / D[j]
+        s = 0.0
+        for k in range(i):
+            s += L[i][k] ** 2 * D[k]
+        D[i] = A[i][i] - s
+        L[i][i] = 1.0
+
+    LT = [[L[j][i] for j in range(n)] for i in range(n)]
+
+    B = [[0.0] * n for i in range(n)]
+    for i in range(n):
+        for j in range(n):
+            s = 0.0
+            for k in range(min(i, j) + 1):
+                s += L[i][k] * D[k] * LT[k][j]
+            B[i][j] = s
+
+    return L, D, LT, B
 
 
-def cholesky(A):
-    L = np.linalg.cholesky(A)
-    return L
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    A = np.array([[2., 2., 3.],
-                  [2., 6., 5.],
-                  [3., 5., 8.]])
-    L = lower_triangular(A)
-    LT = get_transpose(L)
-    cholesky_with_diagonal_out(A)
-    print("-------------------")
+    A = [[4, 6, 10], [6, 25, 19], [10, 19, 94]]
 
+    '''
+    L = np.linalg.cholesky(A)
+
+    D=get_diagonal_matrix(L)
+    LT=get_transpose(L)
+    L=change_diagonal(L)
+    print(A)
+    print(D)
+    print(L)
+    print(LT)
+    print(multiply_matrices(multiply_matrices(L,D),LT))
+    '''
+
+    L, D, LT, B = cholesky_decomposition(A)
+    n = len(A)
+    det_L = 1.0
+    for i in range(n):
+        det_L *= L[i][i]
+
+    det_D = 1.0
+    for i in range(n):
+        det_D *= D[i]
+
+    det_LT = 1.0
+    for i in range(n):
+        det_LT *= L[i][i]
+
+    det_A = det_L * det_D * det_LT
+    print("A= ",A)
+    print("L = ", L)
+    print("D = ", D)
+    print("LT = ", LT)
+    print("B = ", B)
+    print("det A = ", det_A)
