d5e95cb743a24615c297b6a56f63d43f1751fb71
Andrie Tablan <andreitablan01@gmail.com>
2023-03-10 21:25:37 +0200

    bonus + the rest

--- Tema2/main.py
+++ Tema2/main.py
@@ -1,32 +1,13 @@
 # LEAGAN DAN ADRIAN - TABLAN ANDREI RAZVAN - TEMA 2
 import numpy as np
+import random
+
+global epsilon
 
 
-def machine_epsilon():
-    eps = np.finfo(float).eps
-    return eps
-
-
-def lower_triangular(A):
-    n = len(A)
-    L = np.zeros((n, n))
-    for i in range(n):
-        for j in range(i + 1):
-            s = sum(L[i][k] * L[j][k] for k in range(j))
-            if (i == j):
-                L[i][j] = np.sqrt(max(A[i][i] - s, 0))
-            else:
-                L[i][j] = (1.0 / L[j][j] * (A[i][j] - s))
-    return L
-
-
-def change_diagonal(L):
-    n = len(L)
-    for i in range(n):
-        for j in range(n):
-            if i == j:
-                L[i][j] = 1
-    return L
+def set_epsilon(number):
+    global epsilon
+    epsilon = number
 
 
 def get_transpose(A):
@@ -41,89 +22,93 @@ def multiply_matrices(A, B):
     return A.dot(B)
 
 
+def verify_matrix(A):
+    for line in A:
+        for element in line:
+            if element < 0:
+                raise ValueError("A is not positive!")
+
+
+def verify_divide(number):
+    global epsilon
+    if abs(number) <= epsilon:
+        raise ValueError("CANNOT DIVIDE!")
+
+
+def random_symmetric_matrix(n):
+    A = [[0.0] * n for i in range(n)]
+    for i in range(n):
+        for j in range(i + 1):
+            A[i][j] = random.randint(1, 100)
+            A[j][i] = A[i][j]
+    return A
+
+
+def random_vector(n):
+    b = [0.0] * n
+    for i in range(n):
+        b[i] = random.randint(1, 100)
+    return b
+
+
 def cholesky_decomposition_with_L_D_LT(A_init):
+    verify_matrix(A_init)
     n = len(A_init)
     A = [[0.0] * n for i in range(n)]
     L = [[0.0] * n for i in range(n)]
     D = [0.0] * n
 
-    print(L)
-    print(D)
     for i in range(n):
         for j in range(i):
             s = 0.0
             for k in range(j):
                 s += L[i][k] * L[j][k] * D[k]
+            verify_divide(D[j])
             L[i][j] = (A_init[i][j] - s) / D[j]
         s = 0.0
         for k in range(i):
             s += L[i][k] ** 2 * D[k]
+
         D[i] = A_init[i][i] - s
         L[i][i] = 1.0
 
     LT = [[L[j][i] for j in range(n)] for i in range(n)]
 
-    B = [[0.0] * n for i in range(n)]
     for i in range(n):
         for j in range(n):
-            s = 0.0
-            for k in range(min(i, j) + 1):
-                s += L[i][k] * D[k] * LT[k][j]
-            B[i][j] = s
+            for k in range(i + 1):
+                A[i][j] += L[i][k] * D[k] * L[j][k]
 
-    return L, D, LT, B
+    return L, D, LT, A
 
 
-def cholesky_solve(A, b):
-    n = len(A)
-    L, D, LT, B = cholesky_decomposition_with_L_D_LT(A)
+def cholesky_decomposition(A_init):
+    verify_matrix(A_init)
+    n = len(A_init)
+    A = [[0.0] * n for i in range(n)]
+    D = [0.0] * n
 
-    # Forward substitution to solve Ly = b
-    y = [0.0] * n
     for i in range(n):
-        s = 0.0
         for j in range(i):
-            s += L[i][j] * y[j]
-        y[i] = (b[i] - s) / L[i][i]
-
-    # Backward substitution to solve LTx = y
-    x = [0.0] * n
-    for i in reversed(range(n)):
+            s = 0.0
+            for k in range(j):
+                s += A[i][k] * A[j][k] * D[k]
+            verify_divide(D[j])
+            A[i][j] = s + ((A_init[i][j] - s) / D[j]) * D[j]
+            A[j][i] = A[i][j]
         s = 0.0
-        for j in range(i + 1, n):
-            s += LT[i][j] * x[j]
-        x[i] = (y[i] - s) / LT[i][i]
+        for k in range(i):
+            s += A[i][k] ** 2 * D[k]
 
-    return x
+        D[i] = A_init[i][i] - s
+        A[i][i] = s + D[i]
+    return A
 
 
-if __name__ == '__main__':
-    A = [[1, 2.5, 3], [2.5, 8.25, 15.5], [3, 15.5, 43]]
-    b = [12, 38, 68]
-
-    A1 = np.array([[4, 2, 1],
-                   [2, 5, 3],
-                   [1, 3, 6]])
-
-    b1 = np.array([4, 7, 9])
-    # A = [[4, 6, 10], [6, 25, 19], [10, 19, 94]]
-    # b = [2, 5, 10]
-
-    '''
-    L = np.linalg.cholesky(A)
-
-    D=get_diagonal_matrix(L)
-    LT=get_transpose(L)
-    L=change_diagonal(L)
-    print(A)
-    print(D)
-    print(L)
-    print(LT)
-    print(multiply_matrices(multiply_matrices(L,D),LT))
-    '''
-
-    L, D, LT, B = cholesky_decomposition_with_L_D_LT(A)
-    n = len(A)
+def calculate_determinant(A_init):
+    verify_matrix(A_init)
+    L, D, LT, A = cholesky_decomposition_with_L_D_LT(A_init)
+    n = len(A_init)
     det_L = 1.0
     for i in range(n):
         det_L *= L[i][i]
@@ -137,30 +122,85 @@ if __name__ == '__main__':
         det_LT *= L[i][i]
 
     det_A = det_L * det_D * det_LT
-    print("A= ", A)
-    print("L = ", L)
-    print("D = ", D)
-    print("LT = ", LT)
-    print("B = ", B)
     print("det A = ", det_A)
-    xChol = cholesky_solve(A, b)
-    print("xChol = ", xChol)
-    residual = np.dot(A, xChol) - b
-    norm_residual = np.linalg.norm(residual, ord=2)
-    print(norm_residual)
-    print("--------------------------")
 
-    # compute the LU decomposition of A
-    L1 = np.linalg.cholesky(A)
-    U1 = get_transpose(L1)
-    print(" L1 = ", L1)
-    print(" U1 = ", U1)
-    print(multiply_matrices(L1, U1))
-    # solve the system Ax=b using linalg.solve()
-    x1 = np.linalg.solve(A, b)
-    print(x1)
 
-    # calculate the norm of the residual vector
-    residual = np.dot(A, x1) - b
+def cholesky_solve(A, b):
+    global epsilon
+    n = len(A)
+    x = [0.0] * n
+    for i in range(n):
+        s = 0.0
+        for j in range(i):
+            s += A[i][j] * x[j]
+        verify_divide(A[i][i])
+        x[i] = (b[i] - s) / A[i][i]
+
+    y = [0.0] * n
+    for i in range(n):
+        s = 0.0
+        for j in range(i + 1, n):
+            s += A[i][j] * y[j]
+        verify_divide(A[i][i])
+        y[i] = (b[i] - s) / A[i][i]
+    return x, y
+
+
+def cholesky_solve_bonus_y(D, z):
+    global epsilon
+    n = len(D)
+    y = [0.0] * n
+    for i in range(n):
+        verify_divide(D[i])
+        y[i] = z[i] / D[i]
+    return y
+
+
+def solve_system_bonus(A_init, b):
+    verify_matrix(A_init)
+    L, D, LT, A = cholesky_decomposition_with_L_D_LT(A_init)
+    z, z1 = cholesky_solve(L, b)
+    y = cholesky_solve_bonus_y(D, z)
+    x1, x = cholesky_solve(LT, y)
+    return x
+
+
+if __name__ == '__main__':
+    m = int(input("m="))
+    set_epsilon(10 ** -m)
+    A_init = [[1, 2.5, 3], [2.5, 8.25, 15.5], [3, 15.5, 43]]
+    b = [12, 38, 68]
+    # n = int(input("n="))
+    # A_init = random_symmetric_matrix(n)
+    # b = random_vector(n)
+
+    A = cholesky_decomposition(A_init)
+    print("A =", A)
+    calculate_determinant(A)
+    x_chol, y_chol = cholesky_solve(A, b)
+    print("b = ", b)
+    print("x_chol = ", x_chol)
+    print("y_chol = ", y_chol)
+
+    residual = np.dot(A, x_chol) - b
     norm_residual = np.linalg.norm(residual, ord=2)
-    print(norm_residual)
+    print("norm_x = ", norm_residual)
+
+    residual = np.dot(A, y_chol) - b
+    norm_residual = np.linalg.norm(residual, ord=2)
+    print("norm_y = ", norm_residual)
+
+    print("-----------Numpy-------------")
+
+    L = np.linalg.cholesky(A_init)
+    U = get_transpose(L)
+    print(" L = ", L)
+    print(" U = ", U)
+    print("A(from numpy)", multiply_matrices(L, U))
+    x = np.linalg.solve(A_init, b)
+    print("x=", x)
+
+    print("-----------Bonus 2-------------")
+
+    x_bonus=solve_system_bonus(A_init,b)
+    print("x bonus = ", x_bonus)
