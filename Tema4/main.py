40fd9c7b3ac07a7acd613344cf84a8a9c2d6070c
Andrie Tablan <andreitablan01@gmail.com>
2023-04-01 15:34:55 +0300

    tema 4

--- Tema4/main.py
+++ Tema4/main.py
@@ -1,16 +1,53 @@
-# This is a sample Python script.
+def read_system(file1, file2):
+    b = []
+    lines = []
+    with open(file1, "r") as file_in:
+        for line in file_in:
+            lines.append(line)
+    n = int(lines[0])
+    a = [[] for i in range(n)]
+    for line in lines:
+        elements = line.split(",")
+        if len(elements) > 1:
+            number = float(elements[0].strip())
+            i = int(elements[1].strip())
+            j = int(elements[2].strip())
+            my_tuple = (number, j)
+            if len(a[i]) != 0:
+                for tuple_from_list in a[i]:
+                    if tuple_from_list[1] == my_tuple[1]:
+                        new_tuple = (tuple_from_list[1] + my_tuple[1], j)
+                        a[i].remove(tuple_from_list)
+                        a[i].append(new_tuple)
+                    else:
+                        a[i].append(my_tuple)
+            else:
+                a[i].append(my_tuple)
+    lines2 = []
+    with open(file2, "r") as file_in:
+        for line in file_in:
+            lines2.append(line)
+    for index in range(1, n):
+        number = float(lines2[index])
+        b.append(number)
+    return n, a, b
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+def verify_null_on_diagonals(a, n):
+    for index in range(1, n):
+        flag1 = 0
+        flag2 = 0
+
+        for element_tuple in a[index]:
+            if element_tuple[1] == index:
+                flag1 = 1
+            if element_tuple[1] + index == n:
+                flag2 = 1
+        if flag1 == 0 and flag2 == 0:
+            return False
+    return True
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    n, a, b = read_system("sis_liniar/a_5.txt", "sis_liniar/b_5.txt")
+    print("The matrix has all elements on the diagonals !=0", verify_null_on_diagonals(a, n))
