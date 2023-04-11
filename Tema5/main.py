9ec71872ceb27fff17592ced9c65523e09114bed
Andrie Tablan <andreitablan01@gmail.com>
2023-04-11 12:24:10 +0300

    first point done

--- Tema5/main.py
+++ Tema5/main.py
@@ -1,10 +1,12 @@
 import numpy as np
 import math
+import random
 
 
 def verify_divide(number):
     if abs(number) <= 10 ** (-6):
         raise ValueError("CANNOT DIVIDE!")
+
 
 def calculate_norm_vectors(x, y):
     n = len(x)
@@ -46,7 +48,6 @@ def read_matrix_only(file1):
                 for k in range(0, len(a[i])):
                     element_list = a[i][k]
                     if element_list[1] == my_list[1]:
-                        print(element_list[0], my_list[0])
                         suma = element_list[0] + my_list[0]
                         a[i][k] = []
                         a[i][k] = [suma, j]
@@ -62,6 +63,43 @@ def read_matrix_only(file1):
     return n, a
 
 
+def generate_random_matrix(n):
+    a = [[] for i in range(n)]
+    for i in range(n):
+        for j in range(i, n):
+            value2 = random.choice([True, False])
+            if value2:
+                number = round(random.uniform(0.001, 1000), 2)
+                new_element_1 = [number, j]
+                a[i].append(new_element_1)
+                if i != j:
+                    new_element_2 = [number, i]
+                    a[j].append(new_element_2)
+    for i in range(n):
+        new_list = sorted(a[i], key=lambda x: x[1])
+        a[i] = []
+        a[i] = [elem for elem in new_list]
+    return a
+
+
+def verify_matrix(a, n):
+    for i in range(n):
+        for element in a[i]:
+            number = element[0]
+            j = element[1]
+            ok = False
+            for element_1 in a[j]:
+                if element_1[1] == i:
+                    ok = True
+                    if abs(element_1[0] - number)>10**(-6):
+                        print(i,j, element_1[0], element[0])
+                        return False
+            if ok is False:
+                print(i, j, element[0])
+                return False
+    return True
+
+
 def verify_null_on_diagonals(a, n):
     for index in range(1, n):
         flag1 = 0
@@ -75,6 +113,10 @@ def verify_null_on_diagonals(a, n):
 
 
 if __name__ == '__main__':
-    n1, a1 = read_matrix_only("sisteme/m_rar_sim_2023_512.txt")
-
-
+    n512, a512 = read_matrix_only("sisteme/m_rar_sim_2023_512.txt")
+    print("The matrix 512 has A=AT:", verify_matrix(a512,n512))
+    n1024, a1024 = read_matrix_only("sisteme/m_rar_sim_2023_1024.txt")
+    print("The matrix 1024 has A=AT:", verify_matrix(a1024, n1024))
+    n2023, a2023 = read_matrix_only("sisteme/m_rar_sim_2023_2023.txt")
+    print("The matrix 2023 has A=AT:", verify_matrix(a2023, n2023))
+    a_generated=generate_random_matrix(6)
