cb4b2eb3c15387e8440d4059721c9ebded244787
Andrie Tablan <andreitablan01@gmail.com>
2023-04-27 21:12:50 +0300

    tema 8

--- Tema8/main.py
+++ Tema8/main.py
@@ -2,34 +2,38 @@ import math
 import numpy as np
 
 
-def secant_method(f_derivative, x0, x, epsilon, max_iterations):
+def secant_method(f, derivate, x0, x, epsilon, max_iterations):
     k = 0
+    flag = 0
     delta = 0
-    g_x = f_derivative(x)
-    g_x0 = f_derivative(x0)
+    g_x = derivate(f, x)
+    g_x0 = derivate(f, x0)
     delta = ((x - x0) * g_x) / (g_x - g_x0)
     if -epsilon <= (g_x - g_x0) <= epsilon:
         if abs(g_x) <= epsilon / 100:
             delta = 0
+            flag = 1
         else:
             delta = 10 ** (-5)
     x0 = x
     x = x - delta
     k += 1
-    while k < max_iterations and delta <= 10 ** 8 and abs(delta >= epsilon):
-        g_x = f_derivative(x)
-        g_x0 = f_derivative(x0)
-        delta = ((x - x0) * g_x) / (g_x - g_x0)
-        if -epsilon <= (g_x - g_x0) <= epsilon:
-            if abs(g_x) <= epsilon / 100:
-                delta = 0
-            else:
-                delta = 10 ** (-5)
-        x0 = x
-        x = x - delta
-        k += 1
-    if abs(delta) >= epsilon:
-        return x
+    if flag == 0:
+        while k < max_iterations and abs(delta) <= 10 ** 8 and abs(delta) >= epsilon:
+            g_x = derivate(f, x)
+            g_x0 = derivate(f, x0)
+            delta = ((x - x0) * g_x) / (g_x - g_x0)
+            if -epsilon <= (g_x - g_x0) <= epsilon:
+                if abs(g_x) <= epsilon / 100:
+                    delta = 0
+                    break
+                else:
+                    delta = 10 ** (-5)
+            x0 = x
+            x = x - delta
+            k += 1
+    if abs(delta) < epsilon:
+        return x,k
     else:
         return "DIVERGENTA"
 
@@ -55,31 +59,84 @@ def f2_derivative(x):
 
 
 def f3(x):
-    return x**4 - 6*x**3 + 13*x - 12*x + 4
+    return x ** 4 - 6 * (x ** 3) + 13 * (x ** 2) - 12 * x + 4
 
 
 def f3_derivative(x):
-    return 4*x**3 - 18*x**2 + 1
+    return 4 * (x ** 3) - 18 * (x ** 2) + 26 * x - 12
+
+
+def derivative(f, x, h=1e-5):
+    return (f(x + h) - f(x)) / h
+
+
+def second_derivative(f, x, h=1e-5):
+    return (derivative(f, x + h, h) - derivative(f, x, h)) / h
+
+
+def derivative_1_1(f, x, h=1e-5):
+    g1 = (3 * f(x) - 4 * f(x - h) + f(x - 2 * h)) / 2 * h
+    return g1
+
+
+def derivative_1_2(f, x, h=1e-5):
+    g2 = (-f(x + 2 * h) + 8 * f(x + h) - 8 * f(x - h) + f(x - 2 * h)) / 12 * h
+    return g2
+
+
+
+def derivative_2(f, x, h=1e-05):
+    F = (-f(x + 2 * h) + 16 * f(x + h) - 30 * f(x) + 16 * f(x - h) - f(x - 2 * h)) / 12 * (h ** 2)
+    return F
 
 
 if __name__ == '__main__':
-    x0 = 0
-    x1 = -0.5
-    epsilon = 10 ** -5
-    max_iterations = 100
-    x_min = secant_method(f1_derivative, x0, x1, epsilon, max_iterations)
-    print(x_min)
-
     x0 = 3.0
     x1 = 4.0
-    epsilon = 10 ** -5
-    max_iterations = 100
-    x_min = secant_method(f2_derivative, x0, x1, epsilon, max_iterations)
-    print(x_min)
+    epsilon = 10 ** -4
+    max_iterations = 1000
+    x_min1,iterations1 = secant_method(f1,derivative_1_1,x0, x1, epsilon, max_iterations)
+    x_min2,iterations2 = secant_method(f1,derivative_1_2,x0, x1, epsilon, max_iterations)
+    print(x_min1, iterations1)
+    print(x_min1, iterations2)
+    if derivative_2(f1, x_min1)>0:
+        print(x_min1, True)
+    else:
+        print(x_min1, False)
+    if derivative_2(f1, x_min2)>0:
+        print(x_min2, True)
+    else:
+        print(x_min2, False)
+    x0 = 0
+    x1 = -0.5
+    epsilon = 10 ** -4
+    max_iterations = 1000
+    x_min1, iterations1 = secant_method(f2, derivative_1_1, x0, x1, epsilon, max_iterations)
+    x_min2, iterations2 = secant_method(f2, derivative_1_2, x0, x1, epsilon, max_iterations)
+    print(x_min1, iterations1)
+    print(x_min1, iterations2)
+    if derivative_2(f2, x_min1) > 0:
+        print(x_min1, True)
+    else:
+        print(x_min1, False)
+    if derivative_2(f2, x_min2) > 0:
+        print(x_min2, True)
+    else:
+        print(x_min2, False)
 
-    x0 = 0.5
+    x0 = 1
     x1 = 2.5
-    epsilon = 10 ** -5
-    max_iterations = 100
-    x_min = secant_method(f3_derivative, x0, x1, epsilon, max_iterations)
-    print(x_min)
+    epsilon = 10 ** -4
+    max_iterations = 1000
+    x_min1, iterations1 = secant_method(f3, derivative_1_1, x0, x1, epsilon, max_iterations)
+    x_min2, iterations2 = secant_method(f3, derivative_1_2, x0, x1, epsilon, max_iterations)
+    print(x_min1, iterations1)
+    print(x_min1, iterations2)
+    if derivative_2(f3, x_min1) > 0:
+        print(x_min1, True)
+    else:
+        print(x_min1, False)
+    if derivative_2(f3, x_min2) > 0:
+        print(x_min2, True)
+    else:
+        print(x_min2, False)
